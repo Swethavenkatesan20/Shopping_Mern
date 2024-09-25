71e7d9569f277f263c7c15e03d54a3b012fcc05d
Swethavenkatesan20 <157340837+Swethavenkatesan20@users.noreply.github.com>
2024-09-25 18:08:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-   origin: ['http://localhost:3000'], 
+   origin: ['http://localhost:3000','https://shopping-mern-theta.vercel.app'], 
    methods: 'GET,POST,PUT,DELETE',
    credentials: true
 }));
