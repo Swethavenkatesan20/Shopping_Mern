a62c2ec3e5329a2791f3f3f7dfa3f6d7e007f5db
Swethavenkatesan20 <157340837+Swethavenkatesan20@users.noreply.github.com>
2024-09-25 15:27:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,7 @@ const router = require('./routes')
 
 
 const app = express()
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
